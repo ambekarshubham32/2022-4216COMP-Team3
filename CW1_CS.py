df4fe12dfdb1e5746d3acce5dc357730a77de44d
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-02-09 18:06:08 +0000

    Displays all the data in the Excel

--- CW1_CS.py
+++ CW1_CS.py
@@ -2,4 +2,6 @@
 import os
 import pandas as pd
 
+ExcelRead=pd.read_excel("C:\Users\Shubham Ambekar\OneDrive - Liverpool John Moores University\Desktop\GitHub\CW1- Computer Science Workshop")
+print(ExcelRead)
 
