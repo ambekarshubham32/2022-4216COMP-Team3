9ccb42df992bfbfac851f2ded02723022a765268
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-02-10 14:36:32 +0000

    Comment

--- CW1_CS.py
+++ CW1_CS.py
@@ -2,6 +2,7 @@
 import os
 import pandas as pd
 
+#Displays the  data from the Excel file
 ExcelRead=pd.read_csv("C:\\Users\\Shubham Ambekar\\OneDrive - Liverpool John Moores University\\Desktop\\GitHub\\CW1- Computer Science Workshop\\WHO-COVID-19-global-data.csv")
 print(ExcelRead)
 
