ab2c93533d16c44f0569ed4a3e052aec658755d8
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-02-22 14:17:33 +0000

    Created  the menu

--- CW1_CS.py
+++ CW1_CS.py
@@ -7,11 +7,23 @@ import pandas as pd
 data=pd.read_csv("C:\\Users\\Shubham Ambekar\\OneDrive - Liverpool John Moores University\\Desktop\\GitHub\\CW1- Computer Science Workshop\\WHO-COVID-19-global-data.csv")
 print(data)
 
+#Menu
+print("--------------Menu--------------")
+print("1.Current trends")
+print("2.Find Information about your country: ")
+print("3.Display all the countries Covid-19 Data ")
+print("4.Compare two countries")
+    
+print("Please enter  the  option number :")
+choosenOption=input()
+option(choosenOption)
 
-
-
+def option(chooseOption):
     
     
+    
+
+
 #def inputFromFile():
 deathToll("india")  
     
