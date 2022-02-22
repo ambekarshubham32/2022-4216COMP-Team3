79699e3f64f27fb557edc9524ad3c79d329f05e9
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-02-22 14:01:40 +0000

    totol number of  Covid 19 death in a specific country

--- CW1_CS.py
+++ CW1_CS.py
@@ -1,8 +1,28 @@
 #Importing essentail libraries
+from ctypes.wintypes import tagRECT
 import os
 import pandas as pd
 
 #Displays the  data from the Excel file
-ExcelRead=pd.read_csv("C:\\Users\\Shubham Ambekar\\OneDrive - Liverpool John Moores University\\Desktop\\GitHub\\CW1- Computer Science Workshop\\WHO-COVID-19-global-data.csv")
-print(ExcelRead)
+data=pd.read_csv("C:\\Users\\Shubham Ambekar\\OneDrive - Liverpool John Moores University\\Desktop\\GitHub\\CW1- Computer Science Workshop\\WHO-COVID-19-global-data.csv")
+print(data)
 
+
+
+
+    
+    
+#def inputFromFile():
+deathToll("india")  
+    
+
+def deathToll(targetcountry):
+    deathToll=0
+    country=data["Cumulative_deaths"]
+    if(tragetCountry==country):
+        deathToll+=tragetCountry
+    else:
+        deathToll+=0
+    print(deathToll)
+        
+        
