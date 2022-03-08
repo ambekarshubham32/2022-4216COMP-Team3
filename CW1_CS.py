7fa4882592371149734eae99560ec0c1d1f3c6df
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-03-08 12:33:52 +0000

    Re arranged the code

--- CW1_CS.py
+++ CW1_CS.py
@@ -1,23 +1,3 @@
-#Importing essentail libraries
-from ctypes.wintypes import tagRECT
-import os
-import pandas as pd
-
-#Displays the  data from the Excel file
-data=pd.read_csv("WHO-COVID-19-global-data.csv")
-print(data)
-
-#Menu
-print("--------------Menu--------------")
-print("1.Current trends")
-print("2.Find Information about your country: ")
-print("3.Display all the countries Covid-19 Data ")
-print("4.Compare two countries")
-    
-print("Please enter  the  option number :")
-choosenOption=int(input())
-option(choosenOption)
-
 def option(chooseOption):
     if chooseOption==1:
         currentTrends()
@@ -58,3 +38,26 @@ def  CompareCountryCovid19():
 #        deathToll+=0
 #    print(deathToll)
         
+
+
+
+#Importing essentail libraries
+from ctypes.wintypes import tagRECT
+import os
+import pandas as pd
+
+#Displays the  data from the Excel file
+data=pd.read_csv("WHO-COVID-19-global-data.csv")
+print(data)
+
+#Menu
+print("--------------Menu--------------")
+print("1.Current trends")
+print("2.Find Information about your country: ")
+print("3.Display all the countries Covid-19 Data ")
+print("4.Compare two countries")
+    
+print("Please enter  the  option number :")
+choosenOption=int(input())
+option(choosenOption)
+
