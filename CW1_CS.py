603a5ea3e3094844d05bdd1157d3d8d34032f048
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-03-08 13:29:14 +0000

    Added more functions

--- CW1_CS.py
+++ CW1_CS.py
@@ -2,35 +2,15 @@
 from ctypes.wintypes import tagRECT
 import os
 import pandas as pd
-
-
-
-
-     
-    
-    
-
-
-#def inputFromFile():
-#deathToll()  
-    
-#def deathToll(targetcountry):
-#    deathToll=0
-#    country=data["Cumulative_deaths"]
-#    if(tragetCountry==country):
-#        deathToll+=tragetCountry
-#    else:
-#        deathToll+=0
-#    print(deathToll)
-        
-
-
-
-
-
 #Displays the  data from the Excel file
 data=pd.read_csv("WHO-COVID-19-global-data.csv")
 print(data)
+
+
+
+
+
+
 
 print("Please enter the country's name below: ")
 country=input()
@@ -43,8 +23,11 @@ coronavirusInfoType=input()
 CoronavirusParameters()
 
 print("Different types of charts to presentation data . ")
-print("1.Line graph \n 2. Bar chart\n 3. Pie chart \n4.Scatter chat \n 5.Cumulative  graph ")
+print("1.Line graph \n 2. Bar chart\n 3. Pie chart \n4.Scatter chat \n5.Cumulative  graph ")
 chartTypes=input()
+
+
+
 chart()
     
     
