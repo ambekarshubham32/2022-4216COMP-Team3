ff9db025b9d46435754a4cdd8c1c8653744c1e94
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-03-08 13:10:15 +0000

    Created functions

--- CW1_CS.py
+++ CW1_CS.py
@@ -34,14 +34,25 @@ print(data)
 
 print("Please enter the country's name below: ")
 country=input()
+selectCountry()
+
 
 print("What type of  Covid 19 related information do you want to know.")
 print("1. Confimed Cases\n 2.Death\n 3. Vaccination")
 coronavirusInfoType=input()
+CoronavirusParameters()
 
 print("Different types of charts to presentation data . ")
 print("1.Line graph \n 2. Bar chart\n 3. Pie chart \n4.Scatter chat \n 5.Cumulative  graph ")
 chartTypes=input()
+chart()
+    
+    
+def chart():
+
+def selectCountry():
+    
+def CoronavirusParameters():
 
 
 
