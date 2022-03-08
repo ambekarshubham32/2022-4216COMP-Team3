34a04da1081ef2bf7ba885b6947c005f3d40a8ea
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-03-08 12:28:45 +0000

    Shorten the path

--- CW1_CS.py
+++ CW1_CS.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 
 #Displays the  data from the Excel file
-data=pd.read_csv("C:\\Users\\Shubham Ambekar\\OneDrive - Liverpool John Moores University\\Desktop\\GitHub\\CW1- Computer Science Workshop\\WHO-COVID-19-global-data.csv")
+data=pd.read_csv("WHO-COVID-19-global-data.csv")
 print(data)
 
 #Menu
