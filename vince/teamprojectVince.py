16649d8b7e0835dae91c18ea3ed240a8a6bab3b3
vince <vincehyc59@gmail.com>
2022-04-01 16:15:05 +0100

    piecode

--- vince/teamprojectVince.py
+++ vince/teamprojectVince.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+
 countries = []
 count = []
-explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 with open('C:\\Users\\User\\OneDrive\\Desktop\\teamproject\\piechart.txt','r') as file:
     data = file.readlines()
 for i in data:
@@ -10,5 +9,5 @@ for i in data:
     print(broken)
     countries.append(broken[0])
     count.append(int(broken[1]))
-plt.pie(countries, explode=explode, labels=countries, autopct='%1.1f%%', shadow=True, startangle = 75)
+plt.pie(countries)
 plt.show()
