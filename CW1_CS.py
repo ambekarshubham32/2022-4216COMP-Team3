16d0151f46ad6c125eb95185c7657c61082e1ee9
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-04-01 19:49:29 +0100

    Removed option 'pie chart code' and 'display all graph code', becauseI don't have time to fix the code

--- CW1_CS.py
+++ CW1_CS.py
@@ -62,7 +62,7 @@ def barGraph(chartTypes,country,coronavirusInfoType):
         print(caseOrDeath)
    
     
-    DisplayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
+    displayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
 
         
   
@@ -104,7 +104,7 @@ def scatterChart(chartTypes,country,coronavirusInfoType):
         print(date)
         print(caseOrDeath)
     
-        DisplayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
+        displayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
         
     
     
@@ -148,7 +148,7 @@ def lineGraph(chartTypes,country,coronavirusInfoType):
 
 
 
-    DisplayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
+    displayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
    
     
     
@@ -197,63 +197,11 @@ def cumulativeGraph(chartTypes,country,coronavirusInfoType):
         print(caseOrDeath)
         
     
-    DisplayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
+    displayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
 
+ 
 
-def allVisualiations(chartTypes,country,coronavirusInfoType):
-    #calls ths function
-    with open('WHO-COVID-19-global-data.csv', 'r') as f:
-        csv_reader = csv.reader(f)
-        # skip the header
-        next(csv_reader)
-        
-        #Two arrays stores the data for x and y axis number
-        date=[]
-        caseOrDeath=[]
-        cumulativeCaseOrDeath=[]
-           
-    
-        
-        # retrieve specific data fromthe file and stores it in an array above 
-        for rowData in csv_reader:
-            if rowData[2]==country:
-                date.append(str(rowData[0]))
-                if (coronavirusInfoType==1 and chartTypes==4):
-                    case=int(rowData[5])
-                    caseOrDeath.append(case)
-                    yAxis="Cases"
-
-                elif coronavirusInfoType==2 and chartTypes==4:
-                    death=int(rowData[7])
-                    caseOrDeath.append(death)
-                    yAxis="Deaths"
-
-                elif coronavirusInfoType==1:
-                    case=int(rowData[4])
-                    caseOrDeath.append(case)
-                    yAxis="Cases"
-
-                elif coronavirusInfoType==2:
-                    death=int(rowData[6])
-                    caseOrDeath.append(death)
-                    yAxis="Deaths"
-        #Test Code below(check if data is added in the array)
-        print(date)
-        print(caseOrDeath)
-    
-    fig, axs = plt.subplots(1,2,3,4)
-    axs[0].set_title("Number of "+yAxis+"in  "+country)
-    axs[1].set_title("Number of "+yAxis+"in  "+country)
-    axs[2].set_title("Number of "+yAxis+"in  "+country)
-    axs[3].set_title("Number of "+yAxis+"in  "+country)
-    axs[0].plt(dates, caseOrDeath)
-    axs[1].bar(dates, caseOrDeath)
-    axs[2].scatter(dates,caseOrDeath)
-    axs[3].plt(dates,caseOrDeath) 
-    
-    plt.show()
-  
-def DisplayVisualisation(chartType,country,yAxis,coronavirusInfoType,date,caseOrDeath):
+def displayVisualisation(chartType,country,yAxis,coronavirusInfoType,date,caseOrDeath):
     #Display all the essentail features of the graph
     plt.xticks(rotation=90)
     plt.grid()
@@ -265,22 +213,16 @@ def DisplayVisualisation(chartType,country,yAxis,coronavirusInfoType,date,caseOr
 
     if  chartType==1:
         plt.plot(date[0::30],caseOrDeath[0::30], marker = 'o', color='green', ms=5)
-
     elif chartType==2:
         plt.bar(date[0::30],caseOrDeath[0::30], color='blue')
-
-    elif chartType==3:
-       
+    elif chartType==3:      
         plt.colorbar()   
         plt.scatter(date[0::30],caseOrDeath[0::30], marker = 'x', color='blue', ms=5)
-
-    
     elif chartType==4:
-        plt.plot(date[0::30],caseOrDeath[0::30], marker = 'o', color='black', ms=5)
-
-    
-    
+        plt.plot(date[0::30],caseOrDeath[0::30], marker = 'o', color='black', ms=5)  
     plt.show()
+    
+
     
 
     
@@ -297,8 +239,7 @@ def countryDataSelection():
         print("1.Confimed Cases\n2.Death")
         coronavirusInfoType=int(input())
         print("Different types of charts to presentation data . ")
-        print("1.Line graph\n2.Bar chart\n3.Scatter chart\n4.Cumulative graph\
-            \n5. All the graph and function in the list ")
+        print("1.Line graph\n2.Bar chart\n3.Scatter chart\n4.Cumulative graph ")
         chartTypes=int(input())
         if coronavirusInfoType==1:
             chartOrGraph(chartTypes,country,coronavirusInfoType)    
@@ -328,12 +269,7 @@ def chartOrGraph(chartTypes,country,coronavirusInfoType):
             break
         elif chartTypes==4:
             cumulativeGraph(chartTypes,country,coronavirusInfoType)
-            break
-        elif chartTypes==5:
-            allVisualiations(chartTypes,country,coronavirusInfoType)
-            break
-            
-        
+            break 
         else:
             print("Invalid graph choice")
                  
