ee350f37b3f075b1259ffce086f0c0871b0937b2
vince <vincehyc59@gmail.com>
2022-04-01 21:58:24 +0100

    bar chart creation

--- vince/teamprojectVince.py
+++ vince/teamprojectVince.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 plt.figure(figsize=(15,15))
 plt.title("Pie Chart of Most Popular Vaccines from a Group of Countries",fontsize = 20)
@@ -18,7 +19,23 @@ for i in data:
 plt.pie(count, explode=explode, labels=countries, autopct='%1.1f%%', startangle = 75, radius= 1,wedgeprops = {"edgecolor" : "black",'linewidth': 1,'antialiased': True})
 labels = ['{0} - {1:1.2f} %'.format(i,j) for i,j in zip(countries, count)]
 plt.axis('equal')
+plt.show()
 
 
+plt.figure(figsize=(15,15))
+plt.title("Bar Chart of Distribution of Vaccines",fontsize = 20)
+
+Countries = []
+count = []
+with open('C:\\Users\\User\\OneDrive\\Desktop\\teamproject\\piechart.txt','r') as file:
+  data = file.readlines()
+for i in data:
+    broken = i.split('\t')
+    print(broken)
+    Countries.append(broken[0])
+    count.append(int(broken[1]))
+    plt.bar(Countries,count)
+    plt.xlabel('Countries')
+    plt.ylabel('Number of Countries ')
 plt.show()
 
