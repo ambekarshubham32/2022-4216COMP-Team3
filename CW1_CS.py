114821d7cf490e6813ace07e30c2f45adc7e3ec8
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-04-01 10:54:54 +0100

    Code Tested. Fixed some bugs.Remove unwanted code. Added  comments. Tried to change branch name, but it didn't work .

--- CW1_CS.py
+++ CW1_CS.py
@@ -2,6 +2,7 @@
 from ctypes.wintypes import tagRECT
 import os
 from tokenize import String
+from types import TracebackType
 from matplotlib.axis import YAxis
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -61,46 +62,52 @@ def barGraph(chartTypes,country,coronavirusInfoType):
         # skip the header
         next(csv_reader)
         
-        #Two arrays stores the data for x and y axis
+        #Two arrays stores the data for x and y axis number
         date=[]
         caseOrDeath=[]
            
-        # read the data from excel file   
-        for rowData in csv_reader:
-            
-               
-            data=rowData[2]
+    
         
+        # retrieve specific data fromthe file and stores it in an array above 
+        for rowData in csv_reader:
             if rowData[2]==country:
                 date.append(str(rowData[0]))
                 if coronavirusInfoType==1:
                     case=int(rowData[4])
                     caseOrDeath.append(case)
-                    yAxisName="Cases"
-                    #ax.set_title("Cumulative Cases", fontsize=14)
+                    yAxis="Cases"
 
                 elif coronavirusInfoType==2:
-                    case=int(rowData[6])
-                    caseOrDeath.append(case)
-                    yAxisName="Death"
+                    death=int(rowData[6])
+                    caseOrDeath.append(death)
+                    yAxis="Death"
 
-                    #ax.set_title("Cumulative Deaths", fontsize=14)
-                else:
-                    print()
-        #print(date)
-       # print(caseOrDeath)
-      
+               
+        #Test Code below(check if data is added in the array)
+        print(date)
+        print(caseOrDeath)
+   
     
-    plt.bar(date, color='green')
-    plt.xlabel("Date")
-    
-    plt.ylabel(" "+yAxisName)
-    plt.title("Number of "+yAxisName+" in "+country)
-    plt.axvline(date)
-    plt.bar(date,caseOrDeath, width=3, color="blue")
-    plt.show()
+    if coronavirusInfoType==1:
+        plt.bar(date[0::90],caseOrDeath[0::90], color='green')
+    elif coronavirusInfoType==2:
+        plt.bar(date[0::90],caseOrDeath[0::90], color='green')
 
-    #plt.xticks(date, caseOrDeath)
+
+
+
+    #Display all the essentail features of the graph
+    plt.grid(True)
+    plt.title("Number of death in  "+country)
+    plt.xlabel('Date-(dd/MM/yyyy)')
+    plt.ylabel(' '+yAxis)  
+  
+    plt.legend(['Cases', 'Deaths'])
+    
+
+    
+ 
+    plt.show()     
                 
         
   
@@ -117,109 +124,103 @@ def scatterChart(chartTypes,country,coronavirusInfoType):
         # skip the header
         next(csv_reader)
         
-        #Two arrays stores the data for x and y axis
+        #Two arrays stores the data for x and y axis number
         date=[]
         caseOrDeath=[]
            
-        #read the data from excel file   
-        for rowData in csv_reader:
-            
-               
-            data=rowData[2]
+    
         
+        # retrieve specific data fromthe file and stores it in an array above 
+        for rowData in csv_reader:
             if rowData[2]==country:
                 date.append(str(rowData[0]))
                 if coronavirusInfoType==1:
                     case=int(rowData[4])
                     caseOrDeath.append(case)
-                    yAxisName="Cases"
-                    #ax.set_title("Cumulative Cases", fontsize=14)
+                    yAxis="Cases"
 
                 elif coronavirusInfoType==2:
-                    case=int(rowData[6])
-                    caseOrDeath.append(case)
-                    yAxisName="Death"
+                    death=int(rowData[6])
+                    caseOrDeath.append(death)
+                    yAxis="Death"
 
-                    #ax.set_title("Cumulative Deaths", fontsize=14)
-                else:
-                    print()
-       # print(date)
-        #print(caseOrDeath)
+              
+        #Test Code below(check if data is added in the array)
+        print(date)
+        print(caseOrDeath)
     
-    plt.xlabel("Date")
+   
+   
+    if coronavirusInfoType==1:
+        plt.scatter(date[0::90],caseOrDeath[0::90], color='green')
+    elif coronavirusInfoType==2:
+        plt.scatter(date[0::90],caseOrDeath[0::90], color='green')
+
+
+
+    #Display all the essentail features of the graph
+    plt.grid(True)
+    plt.title("Number of death in  "+country)
+    plt.xlabel('Date-(dd/MM/yyyy)')
+    plt.ylabel(' '+yAxis)  
+  
+    plt.legend(['Cases', 'Deaths'])
+    plt.show()     
     
-    plt.ylabel(" "+yAxisName)
-    plt.title("Number of "+yAxisName.lower()+" "+country)
-
-    plt.scatter(date, caseOrDeath)
-    plt.show()
-
+    
+    
       
 
             
 def lineGraph(chartTypes,country,coronavirusInfoType):
+
     #calls ths function
     with open('WHO-COVID-19-global-data.csv', 'r') as f:
         csv_reader = csv.reader(f)
         # skip the header
         next(csv_reader)
         
-        #Two arrays stores the data for x and y axis
+        #Two arrays stores the data for x and y axis number
         date=[]
         caseOrDeath=[]
            
     
         
-        # read the data from excel file   
+        # retrieve specific data fromthe file and stores it in an array above 
         for rowData in csv_reader:
             if rowData[2]==country:
                 date.append(str(rowData[0]))
                 if coronavirusInfoType==1:
                     case=int(rowData[4])
                     caseOrDeath.append(case)
-                    yAxisName="Cases"
+                    yAxis="Cases"
                     #ax.set_title("Cumulative Cases", fontsize=14)
 
                 elif coronavirusInfoType==2:
-                    case=int(rowData[6])
-                    caseOrDeath.append(case)
-                    yAxisName="Death"
+                    death=int(rowData[6])
+                    caseOrDeath.append(death)
+                    yAxis="Death"
 
-                    #ax.set_title("Cumulative Deaths", fontsize=14)
-                else:
-                    print()
-       
-        else:
-            print()
-    print(date)
-    print(caseOrDeath)
-    #ax.set_xticks(caseOrDeath.why[0::60])
-    fig, ax = plt.subplots(figsize=18)
+                  
+        #Test Code below(check if data is added in the array)    
+        print(date)
+        print(caseOrDeath)
+    
     if coronavirusInfoType==1:
-        plt.plot(caseOrDeath[0::60],caseOrDeath[0::60],'*g--')
+        plt.plot(date[0::90],caseOrDeath[0::90],'*g--')
     elif coronavirusInfoType==2:
-        plt.plot(caseOrDeath[0::60],caseOrDeath[0::60],'rD--')
-            
+        plt.plot(date[0::90],caseOrDeath[0::90],'rD--')
 
-    ax.set_title("Cases", fontsize=3)
-    ax.set_title("Deaths", fontsize=3)
-    fig, ax = plt.subplots(figsize=(12, 24))
-    fig.suptitle("Number of "+yAxisName+" in " + country, fontsize=18)
 
+
+    #Display all the essentail features of the graph
+    plt.grid(True)
+    plt.title("Number of "+yAxis+" in  "+country)
+    plt.xlabel('Date -(dd/MM/yyyy)')
+    plt.ylabel(' '+yAxis)  
   
     plt.legend(['Cases', 'Deaths'])
     
-
-    ax.set_facecolor('xkcd:seashell')
-
-    ax.xaxis.grid()
-    ax.yaxis.grid()
-   
-   
-    fig.suptitle("Numbers of "+yAxisName+" in "+country, fontsize=25)
-    ax.set_xlabel("Date", fontsize=12)
-    ax.set_ylabel(" "+yAxisName, fontsize=12)
- 
     plt.show()     
     
     
@@ -234,6 +235,8 @@ def lineGraph(chartTypes,country,coronavirusInfoType):
 
 
 
+
+
 def cumulativeGraph(chartTypes,country,coronavirusInfoType):
     
     with open('WHO-COVID-19-global-data.csv', 'r') as f:
@@ -244,103 +247,98 @@ def cumulativeGraph(chartTypes,country,coronavirusInfoType):
         #Two arrays stores the data for x and y axis
         date=[]
         caseOrDeath=[]
-   
         
-    # read the data from excel file   
+        # retrieve specific data fromthe file and stores it in an array above 
         for rowData in csv_reader:
-            
-               
-            data=rowData[2]
-        
             if rowData[2]==country:
                 date.append(str(rowData[0]))
                 if coronavirusInfoType==1:
-                    case=int(rowData[4])
+                    case=int(rowData[5])
                     caseOrDeath.append(case)
-                    yAxisName="Cases"
-                    #ax.set_title("Cumulative Cases", fontsize=14)
+                    yAxis="Cases"
+                  
 
                 elif coronavirusInfoType==2:
-                    case=int(rowData[6])
-                    caseOrDeath.append(case)
-                    yAxisName="Death"
+                    death=int(rowData[7])
+                    caseOrDeath.append(death)
+                    yAxis="Death"
 
-                    #ax.set_title("Cumulative Deaths", fontsize=14)
-                else:
-                    print()
-        #print(date)
-        #print(caseOrDeath)
-       
-        fig, ax = plt.subplots()
-        ax.set_title("Cumulative Cases", fontsize=14)
-        ax.set_title("Cumulative Deaths", fontsize=14)
+             
+        #Test Code below(check if data is added in the array)
+        print(date)
+        print(caseOrDeath)
+    
+    if coronavirusInfoType==1:
+        plt.plot(date[0::90],caseOrDeath[0::90],'*g--')
+    elif coronavirusInfoType==2:
+        plt.plot(date[0::90],caseOrDeath[0::90],'rD--')
+
+
+
+    #Display all the essentail features of the graph
+    plt.grid(True)
+    plt.title("Number of death in  "+country)
+    plt.xlabel('Date -(dd/MM/yyyy)')
+    plt.ylabel(' '+yAxis)  
+  
+    plt.legend(['Cases', 'Deaths'])
+    
 
    
-        fig.suptitle("Figure title", fontsize=18)
-        ax.set_xlabel("Value", fontsize=12)
-        ax.set_ylabel("Square of Value", fontsize=12)
-    
-        ax.plot(date, ax, 'mD:', label="squares")
-        ax.legend() 
-        plt.show()        
+ 
+    plt.show()     
                     
 
-              
- 
-    
 
 
 
 
+#User Input's country information and the graphor chart type
 def countryDataSelection():
-        #validInput=True;
-        #User inputs
     
         print("Please enter the country's name below: ")
         country=input()
         print("What type of  Covid 19 related information do you want to know.")
         print("1.Confimed Cases\n2.Death")
         coronavirusInfoType=int(input())
-    
+        print("Different types of charts to presentation data . ")
+        print("1.Line graph\n2.Bar chart\n3.Scatter chart\n4.Cumulative  graph\n ")
+        chartTypes=int(input())
         if coronavirusInfoType==1:
-            print("Different types of charts to presentation data . ")
-            print("1.Line graph\n2.Bar chart\n3.Scatter hart\n4.Cumulative  graph ")
-            chartTypes=int(input())
-            chart(chartTypes,country,coronavirusInfoType)    
+            chartOrGraph(chartTypes,country,coronavirusInfoType)    
         elif coronavirusInfoType==2:    
-            print("Different types of charts to presentation data . ")
-            print("1.Line graph\n2.Bar chart\n3.Scatter chart\n4.Cumulative graph ")
-            chartTypes=int(input())
-            chart(chartTypes,country,coronavirusInfoType)    
+            chartOrGraph(chartTypes,country,coronavirusInfoType)    
             
         else:
-            print()
-            #invaldInput=False
+            print("Invalid Input")
+          
 
 
 
   
     
            
-  
-def chart(chartTypes,country,coronavirusInfoType):
-   # validInput=True;
+ #runs the graph or chart function 
+def chartOrGraph(chartTypes,country,coronavirusInfoType): 
     
     while True:        
         if chartTypes==1:
             lineGraph(chartTypes,country,coronavirusInfoType)
+            break
         elif chartTypes==2:
             barGraph(chartTypes,country,coronavirusInfoType)
+            break
         elif chartTypes==3:
             scatterChart(chartTypes,country,coronavirusInfoType)
+            break
         elif chartTypes==4:
             cumulativeGraph(chartTypes,country,coronavirusInfoType)
+            break
         else:
             print("Invalid graph choice")
-            break     
+                 
 
 #Menu
-optionValid=""
 while(True):
     print("Menu")
     print("1 - Top 5 vaccines\n2 - Finding data by country\n3 - What time of year had the most deaths/vaccines?\
@@ -349,6 +347,7 @@ while(True):
     print("Please enter the number  here:")
     menuOption=input()
 
+    #Executes the option code of the function
     if menuOption=="1":
         topFiveVaccine()
     elif menuOption=="2":
