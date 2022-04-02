1728f01a4a836159c89c360c9fbb807ed9ba26b8
ambekarshubham30@gmail.com <ambekarshubham30@gmail.com>
2022-04-02 18:37:15 +0100

    Fixed  some bugs

--- CW1_CS.py
+++ CW1_CS.py
@@ -28,8 +28,29 @@ vaccineData=pd.read_csv("vaccination-data.csv")
 #def comparisonTotalDeathOrVaccineOnSpecificCountries():
 
 #Shubham
+def displayVisualisation(chartType,country,yAxis,date,caseOrDeath):
+    #Display all the essentail features of the graph
+    plt.xticks(rotation=90)
+    plt.grid()
+    plt.title("Number of "+yAxis.lower()+" in  "+country, fontsize=18)
+    plt.xlabel('Date -(dd/MM/yyyy)', fontsize=18)
+    plt.ylabel(' '+yAxis,fontsize=18)  
+  
+    plt.legend(['Cases', 'Deaths'])
 
+    if  chartType==1:
+        plt.plot(date[0::30],caseOrDeath[0::30], marker = 'o', color='green')
+    elif chartType==2:
+        plt.bar(date[0::30],caseOrDeath[0::30], color='blue')
+    elif chartType==3:       
+        plt.scatter(date[0::30],caseOrDeath[0::30], marker = 'x', color='blue')
+    elif chartType==4:
+        plt.plot(date[0::30],caseOrDeath[0::30], marker = 'o', color='black') 
+    plt.show()
+    
 def loadData(chartTypes,country,coronavirusInfoType):
+    
+    
     with open('WHO-COVID-19-global-data.csv', 'r') as f:
         csv_reader = csv.reader(f)
         # skip the header
@@ -38,24 +59,13 @@ def loadData(chartTypes,country,coronavirusInfoType):
         #Two arrays stores the data for x and y axis number
         date=[]
         caseOrDeath=[]
-        cumulativeCaseOrDeath=[]
-    
-
+     
         # retrieve specific data fromthe file and stores it in an array above 
+        
         for rowData in csv_reader:
             if rowData[2]==country:
                 date.append(str(rowData[0]))
-                if (coronavirusInfoType==1 and chartTypes==4):
-                    case=int(rowData[5])
-                    caseOrDeath.append(case)
-                    yAxis="Cases"
-
-                elif coronavirusInfoType==2 and chartTypes==4:
-                    death=int(rowData[7])
-                    caseOrDeath.append(death)
-                    yAxis="Deaths"
-
-                elif coronavirusInfoType==1:
+                if coronavirusInfoType==1:
                     case=int(rowData[4])
                     caseOrDeath.append(case)
                     yAxis="Cases"
@@ -64,78 +74,50 @@ def loadData(chartTypes,country,coronavirusInfoType):
                     death=int(rowData[6])
                     caseOrDeath.append(death)
                     yAxis="Deaths"
+                elif (coronavirusInfoType==1 and chartTypes==4):
+                    case=int(rowData[5])
+                    caseOrDeath.append(case)
+                    yAxis="Cases"
+
+                elif (coronavirusInfoType==2 and chartTypes==4):
+                    death=int(rowData[7])
+                    caseOrDeath.append(death)
+                    yAxis="Deaths"
                 
-        #Test Code below(check if data is added in the array)
+                
+            #Test Code below(check if data is added in the array)
         print(date)
         print(caseOrDeath)
         
-        displayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
+    displayVisualisation(chartTypes,country,yAxis,coronavirusInfoType,date,caseOrDeath)
     
-       
-   
-    
-
-
-
-
-
-
  
-
-def displayVisualisation(chartType,country,yAxis,coronavirusInfoType,date,caseOrDeath, case, death):
-    #Display all the essentail features of the graph
-    plt.xticks(rotation=90)
-    plt.grid()
-    plt.title("Number of "+yAxis.lower()+" in  "+country)
-    plt.xlabel('Date -(dd/MM/yyyy)')
-    plt.ylabel(' '+yAxis)  
-  
-    plt.legend(['Cases', 'Deaths'])
-
-    if  chartType==1:
-        plt.plot(date[0::30],caseOrDeath[0::30], marker = 'o', color='green', ms=5)
-    elif chartType==2:
-        plt.bar(date[0::30],caseOrDeath[0::30], color='blue')
-    elif chartType==3:       
-        plt.scatter(date[0::30],caseOrDeath[0::30], marker = 'x', color='blue')
-    elif chartType==4:
-        plt.plot(date[0::30],caseOrDeath[0::30], marker = 'o', color='black', ms=5) 
-  
-    plt.show()
-    
-
-    
-
-    
-
 
 
 
 #User Input's country information and the graphor chart type
 def countryDataSelection():
-    
-        print("Please enter the country's name below: ")
+
+        print("Please enter the country's name below(first letter uppercase): ")
         country=input()
         print("What type of  Covid 19 related information do you want to know.")
         print("1.Confimed Cases\n2.Death")
-        coronavirusInfoType=int(input())
+        coronavirusInfoType=input()
         print("Different types of charts to presentation data . ")
         print("1.Line graph\n2.Bar chart\n3.Scatter chart\n4.Cumulative graph")
         chartTypes=int(input())
         loadData(chartTypes,country,coronavirusInfoType)
-          
-
-
 #Menu
 while(True):
     #Displays the menu
     print("Menu")
-    print("1 - Top 5 vaccines\n2 - Finding data by country\n3 - What time of year had the\
+    print("1 - Top 5 vaccines\n2 - Find data by country\n3 - What time of year had the\
         most deaths vaccines?\n4 - Type of vaccines used\n5 - Popular vaccine per\
-        country\n6 - Comparing total deaths and vaccines of specific countries\n7 - Quit")
+        country\n6 - Comparing total deaths and vaccines of specific countries\n7\
+        - Quit\nPlease enter the number  here:")
 
     #user enter's the choosen option
-    print("Please enter the number  here:")
+    
     menuOption=input()
 
     #Runs a function depending on the user's input
@@ -155,4 +137,4 @@ while(True):
     elif menuOption=="7":
         break 
     else:
-        print("Invalid Input. Please enter a number from the menu.")
+        print("Invalid Input.Please enter a number from the menu.")
