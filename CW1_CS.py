7bb7174b03185ec2a6dc85b033b7bc55fb768ada
harryh2022 <97961570+harryh2022@users.noreply.github.com>
2022-04-04 19:05:33 +0100

    Update CW1_CS.py
    added my section of the code

--- CW1_CS.py
+++ CW1_CS.py
@@ -13,12 +13,47 @@ casesAndDeathData=pd.read_csv("WHO-COVID-19-global-data.csv")
 vaccineData=pd.read_csv("vaccination-data.csv")
 
 #Harry
-#def topFiveVaccine():
+def vaccineComparisonData():
+
+    vaccination_data = pd.read_csv("vaccination-data.csv")
+    file = vaccination_data[['COUNTRY', 'TOTAL_VACCINATIONS']]
+    #begins reading from the file
+
+    print("Welcome! Please input the name of a country :")
+    choice = input()
+
+    countryData = file[file.COUNTRY == choice]
+    print(countryData)
+    #requests the user's first choice and prints it
+
+    print("Please input a country to compare this to :")
+    second_choice = input()
+
+    second_country = file[file.COUNTRY == second_choice]
+    print(second_country)
+    #requests the user's second choice and prints it
+
+    fig, ax = plt.subplots(figsize=(5,6))
+    fig.suptitle(choice + " vs " + second_choice + " Total Vaccinations", fontsize=18)
+    plt.xlabel('Countries', fontsize=18)
+    plt.ylabel('Vaccinations', fontsize=18)
+    #sets the dimensions and titles of the bar chart
+
+    ax.bar(countryData.COUNTRY, countryData.TOTAL_VACCINATIONS)
+    ax.bar(second_country.COUNTRY, second_country.TOTAL_VACCINATIONS)
+    plt.legend([choice, second_choice])
+    #plots the data onto the bar chart
+
+    ax.set_facecolor('xkcd:grey')
+    fig.patch.set_facecolor('xkcd:white')
+    #changes the colours of some elements of the bar chat
+
+    plt.show()
+    #shows the bar chart
     
 
     
-#Harry
-#def topFiveVaccine(): 
+ 
 #Lydia    
 def deathAndVaccine(): 
 #Reading files
