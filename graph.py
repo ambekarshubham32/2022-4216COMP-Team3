0fcd660edef48af99145a714ccad479659e24f53
lydiaankers8@gmail.com <lydiaankers8@gmail.com>
2022-04-04 11:44:57 +0100

    finished

--- graph.py
+++ graph.py
@@ -1,11 +1,10 @@
-
+#imported libraries
 from tkinter.font import BOLD
 import pandas as pd
 import matplotlib.pyplot as plt
 
 #reading from the files
 covid= pd.read_csv("WHO-COVID-19-global-data.csv")
-vaccination_data= pd.read_csv("vaccination-data.csv")
 
 #user input for the country of choice
 print("please input a country")
@@ -13,7 +12,6 @@ country_selected = input()
 
 #linking files to country selected
 country_data = covid[covid.Country==country_selected]
-vaccines = vaccination_data[vaccination_data.COUNTRY==country_selected]
 
 #creating death cases graph
 fig,ax = plt.subplots()
@@ -22,12 +20,10 @@ fig.suptitle("Number of Deaths from " + country_selected, fontsize=18, weight=BO
 #plotting and labeling country death graph
 ax.plot(country_data.why[0::70], country_data.Cumulative_deaths[0::70], "r--",linewidth=3)
 ax.set_xlabel("Dates", fontsize=12, weight=BOLD)
-ax.set_ylabel("Deaths Reported", fontsize=12, weight=BOLD)
+ax.set_ylabel("Number of Deaths Reported", fontsize=12, weight=BOLD)
 
-#adding colour to the graph
+#graph additions/colours
 ax.set_facecolor("xkcd:pale")
-
-#additions to graph for easier understanding/viewing
 plt.tight_layout()
 ax.grid(True)
 plt.legend("deaths")
